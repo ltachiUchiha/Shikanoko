71eefea99db697cf8150875210af64b25208946c
ltachiUchiha <itachiuchihadev@gmail.com>
2025-09-11 16:54:33 +0300

    Misc changes

--- app/src/main/java/com/shikanoko/study/screens/MainScreen.kt
+++ app/src/main/java/com/shikanoko/study/screens/MainScreen.kt
@@ -5,8 +5,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import com.shikanoko.study.repositories.NihongoXMLRepository
 
 @Composable
 fun MainScreen () {
@@ -14,7 +12,5 @@ fun MainScreen () {
         modifier = Modifier
             .fillMaxSize()
     ) {
-        val nihonCSVRepository = NihongoXMLRepository()
-        nihonCSVRepository.getAllWords(LocalContext.current)
     }
 }
